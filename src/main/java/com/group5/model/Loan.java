774dd563f1e550009a5b0a83082ba8cbb077de9c
npmcatipon <npmcatipon@bpi.com.ph>
2026-02-04 09:00:28 +0800

    feat:borrow book. revise Loan table and Loan model.

--- src/main/java/com/group5/model/Loan.java
+++ src/main/java/com/group5/model/Loan.java
@@ -3,29 +3,33 @@ package com.group5.model;
 public class Loan {
 
 	private String loanId;
-	private User user;
-	private Book book;
+	private String userId;
+	private String bookId;
 	
+	public Loan() {}
 	
+	public Loan(String loanId, String userId, String bookId) {
+		super();
+		this.loanId = loanId;
+		this.userId = userId;
+		this.bookId = bookId;
+	}
 	public String getLoanId() {
 		return loanId;
 	}
 	public void setLoanId(String loanId) {
 		this.loanId = loanId;
 	}
-	public User getUser() {
-		return user;
+	public String getUserId() {
+		return userId;
 	}
-	public void setUser(User user) {
-		this.user = user;
+	public void setUserId(String userId) {
+		this.userId = userId;
 	}
-	public Book getBook() {
-		return book;
+	public String getBookId() {
+		return bookId;
 	}
-	public void setBook(Book book) {
-		this.book = book;
+	public void setBookId(String bookId) {
+		this.bookId = bookId;
 	}
-	
-
-	
 }
