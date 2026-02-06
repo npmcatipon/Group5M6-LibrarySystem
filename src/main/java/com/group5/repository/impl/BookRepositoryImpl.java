37e2c30bdff0e102a8552a9481c307fc4fd75c56
npmcatipon <npmcatipon@bpi.com.ph>
2026-02-06 10:53:16 +0800

    created repository and bookrepository

---
+++ src/main/java/com/group5/repository/impl/BookRepositoryImpl.java
@@ -0,0 +1,40 @@
+package com.group5.repository.impl;
+
+import java.util.List;
+
+import com.group5.model.Book;
+import com.group5.repository.Repository;
+
+public class BookRepositoryImpl implements Repository<Book, Long> {
+
+	@Override
+	public Book save(Book entity) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void delete(Book entity) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteById(Long id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Book findById(Long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Book> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
